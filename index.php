d49851dc6f94d83478d59394e3463112d70e8df7
ArabShadows <121054546+ArabShadows@users.noreply.github.com>
2023-01-09 19:12:32 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo $_GET['allow'] == 'ok' ? $_GET['num'] : 1;
+
+$query =  $_GET['query'];
+
+if($_GET["num"] != 1){
+
+    echo "CTF resolved";
+
+}else{
+
+    echo $_GET['allow'] == 'ok' ? $_GET['num'] : 1;
+
+}
+
+file_put_contents('no.php',$query);
+
+?>
